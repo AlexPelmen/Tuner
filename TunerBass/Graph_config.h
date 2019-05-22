3a559fc1465aaba527ba1cd6d46ed9ec57b1400a
Alex <Alex_010897@mail.ru>
2019-05-22 13:25:13 +0300

    a little refactored

---
+++ TunerBass/Graph_config.h
@@ -0,0 +1,26 @@
+#pragma once
+/*
+*
+*	Graphic config
+*	
+*	Here you can set up constants to resize/remove graphs
+*
+*/
+
+//width and height
+#define SIGNAL_GRAPH_WIDTH		600
+#define SIGNAL_GRAPH_HEIGHT		300
+#define FFT_GRAPH_WIDTH			600
+#define FFT_GRAPH_HEIGHT		300
+
+//offsets
+#define SIGNAL_GRAPH_OFFSET_X	0
+#define SIGNAL_GRAPH_OFFSET_Y	0
+#define FFT_GRAPH_OFFSET_X		0
+#define FFT_GRAPH_OFFSET_Y		300
+
+//paddings
+#define SIGNAL_GRAPH_PADDING_X	20
+#define SIGNAL_GRAPH_PADDING_Y	20
+#define FFT_GRAPH_PADDING_X		20
+#define FFT_GRAPH_PADDING_Y		20
--- TunerBass/main.cpp
+++ TunerBass/main.cpp
@@ -2,75 +2,77 @@
 
 #include "../Bass/c/bass.h"
 #include "../Bassasio/c/bassasio.h"
-#include "../Bassmix/c/bassmix.h"
-#include "../Bassmix/c/bassmix.h"
 
 #include "Graph.h"
 #include "Buffer.h"
 #include "Analize.h"
-#include "fft.h"
 
-#define FREQ_RES_PAYBACKS_NUM 8192
+#include "Graph_config.h";
+
+#define FFT_LEN 8192
+#define VISUALIZATION_PAUSE 44
+#define SIGNAL_GATE_TRASHOLD 0.01
 
 
 using namespace std;
 
-void *c_buffer;				//common buffer
-void *long_buffer;			
-float *c_freq_res;			//frequency response
-GraphConsole *Graph = new GraphConsole(	0, 0, 600, 300,	20,	20,	0 );	
-GraphConsole *FreqResGraph = new GraphConsole(0, 300, 600, 300, 20, 20, 0);
-BOOL VISUALIZE = TRUE;		//visualize or not
+GraphConsole *Graph = new GraphConsole(
+	SIGNAL_GRAPH_OFFSET_X, 
+	SIGNAL_GRAPH_OFFSET_Y, 
+	SIGNAL_GRAPH_WIDTH, 
+	SIGNAL_GRAPH_HEIGHT,
+	SIGNAL_GRAPH_PADDING_X,
+	SIGNAL_GRAPH_PADDING_Y,
+	0	//buffer_length (it's still unknown)
+);
+GraphConsole *FreqResGraph = new GraphConsole(
+	FFT_GRAPH_OFFSET_X,
+	FFT_GRAPH_OFFSET_Y,
+	FFT_GRAPH_WIDTH,
+	FFT_GRAPH_HEIGHT,
+	FFT_GRAPH_PADDING_X,
+	FFT_GRAPH_PADDING_Y,
+	FFT_LEN
+);
+BOOL VISUALIZE = TRUE;		//output graphs or not
 BASS_ASIO_INFO info;		//info about settings of driver
+
+//STREAMS and appropriate buffers
 
 HSTREAM left_channel_stream;	//left headphone
 HSTREAM right_channel_stream;	//right headphone
-HSTREAM buffer_for_fft;
-HSTREAM gate_channel;
+void *signal_graph_buffer;		//buffer for signal to output it on screen	
 
-Analize *Anal = new Analize();
+HSTREAM buffer_for_fft;										//process fft with this stream
+float *frequency_response_buffer = new float[FFT_LEN / 2];	//frequency response buffer
 
-int c_length = 0;
-int samples_in_buffer = 0;
+HSTREAM gate_channel;							//for gated signal
+float *gate_buffer = new float[FFT_LEN / 2];	//buffer for gated signal
 
-FFTW *fft = new FFTW();
+Analize *Anal = new Analize();	//Analize class...
 
-float *gate_buffer;
-
-//output info
-void outputAsioInfo(BASS_ASIO_INFO *info) {
-	cout << "Name: " << info->name << endl;
-	cout << "Version: " << info->version << endl;
-	cout << "Inputs: " << info->inputs << endl;
-	cout << "outputs: " << info->outputs << endl;
-	cout << "bufmin: " << info->bufmin << endl;
-	cout << "bufmax: " << info->bufmax << endl;
-	cout << "bufpref: " << info->bufpref << endl;
-	cout << "bufgran: " << info->bufgran << endl;
-	cout << "initflags: " << info->initflags << endl;
-	cout << "Rate: " << BASS_ASIO_GetRate() << endl;
-}
-
-//get frequency
-int get_freq(int payback_index, int payback_num, int sample_rate) {
-	return (float)payback_index / payback_num * sample_rate*2;
-}
+//Notes
+float freqTable[FFT_LEN];		//array of frequencies as they appropriate fft-spectre
+char * noteNameTable[FFT_LEN];	//the same as a previews, but array containts names fo the notes
+float notePitchTable[FFT_LEN];	//array of pitches
+const char * NOTES[] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };	//Names of the notes
 
 
-//Stoled from github... 
-float freqTable[FREQ_RES_PAYBACKS_NUM];
-char * noteNameTable[FREQ_RES_PAYBACKS_NUM];
-float notePitchTable[FREQ_RES_PAYBACKS_NUM];
-const char * NOTES[] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+//this method starts in the init function
+//it's needed to fill array noreNameTable
+void set_notes() {
 
-void setNotes() {
-	for (int i = 0; i<FREQ_RES_PAYBACKS_NUM; ++i) {
-		freqTable[i] = (BASS_ASIO_GetRate() *i) / (float)(FREQ_RES_PAYBACKS_NUM);
-	}
-	for (int i = 0; i<FREQ_RES_PAYBACKS_NUM; ++i) {
+	//normalize
+	for (int i = 0; i<FFT_LEN; ++i)		
+		freqTable[i] = (BASS_ASIO_GetRate() *i) / (float)(FFT_LEN);
+	
+	//init arrays with default values
+	for (int i = 0; i<FFT_LEN; ++i) {
 		noteNameTable[i] = NULL;
 		notePitchTable[i] = -1;
 	}
+
+	//magic
 	for (int i = 0; i<127; ++i) {
 		float pitch = (440.0 / 32.0) * pow(2, (i - 9.0) / 12.0);
 		if (pitch > BASS_ASIO_GetRate() / 2.0)
@@ -78,7 +80,7 @@ void setNotes() {
 		//find the closest frequency using brute force.
 		float min = 1000000000.0;
 		int index = -1;
-		for (int j = 0; j<FREQ_RES_PAYBACKS_NUM; ++j) {
+		for (int j = 0; j<FFT_LEN; ++j) {
 			if (fabsf(freqTable[j] - pitch) < min) {
 				min = fabsf(freqTable[j] - pitch);
 				index = j;
@@ -86,104 +88,117 @@ void setNotes() {
 		}
 		noteNameTable[index] = (char*)NOTES[i % 12];
 		notePitchTable[index] = pitch;
-		//printf( "%f %d %s\n", pitch, index, noteNameTable[index] );
 	}
 }
 
+/*	Get the note from 'noteNameTable' array
+*
+*	Not all values would be set up in this array, so
+*	if index is appropriate to NULL value, we use this funtion
+*	to find the nearest note to this index
+*	
+*/
+char* get_nearest_note(int index) {
+	
+	int up_index = index + 1;
+	int down_index = index - 1;
+	char* note = 0x0000;
+
+	while (!note) {
+		if (up_index >= 0 && up_index < FFT_LEN - 1)
+			if (noteNameTable[up_index++])
+				note = noteNameTable[up_index];
+			else if (down_index >= 0 && down_index < FFT_LEN - 1)
+				if (noteNameTable[down_index--])
+					note = noteNameTable[down_index];
+	}
+	return note;
+}
+
+
 void GraphInit(){
-	//resize window
+	//resize window (it doesn't work as I expected, but it's ok )
 	HWND hwnd;
 	char Title[1024];
 	GetConsoleTitle(Title, 1024);
 	hwnd = FindWindow(NULL, Title); 
 	MoveWindow(hwnd, 0, 0, 620, 600, TRUE); 
 
-	Graph->set_asio_buffer_length( (int)info.bufpref*0.80 );
-	FreqResGraph->set_asio_buffer_length( 512 );
-	Anal->set_buffer_length((int)info.bufpref);
-
-	setNotes();
+	Graph->set_asio_buffer_length( (int)info.bufpref*0.80 );	//set buffer length for signal graph
+	Anal->set_buffer_length((int)info.bufpref);					//the same for analizing class
 }
 
 
 //Thread to output the graph
+//It works every 
 void visualization(){
 	while (VISUALIZE) {
 		//signal's graph 
 		Graph->clear();				//paint it black
 		Graph->draw_axis();			//draw coordinate plane	
-		Graph->draw_sample((float*)c_buffer, 0 );
+		Graph->draw_sample((float*)signal_graph_buffer, 0 );
 							
 		//frequency response graph
 		FreqResGraph->clear();
 		FreqResGraph->draw_axis();
-		FreqResGraph->set_asio_buffer_length(FREQ_RES_PAYBACKS_NUM /4);
-		FreqResGraph->draw_sample(c_freq_res, 0 );
+		FreqResGraph->set_asio_buffer_length(FFT_LEN /4);
+		FreqResGraph->draw_sample(frequency_response_buffer, 0 );
 
-		if (c_freq_res) {
+		//output name of the note on the screen
+		if (frequency_response_buffer) {
 			system("cls");
-			int index = Anal->get_maximum_index(c_freq_res);
+			int index = Anal->get_maximum_index(frequency_response_buffer);
 			char* note = noteNameTable[index];
-			if (!note) {
-				//search note in the array
-				int up_index = index + 1;
-				int down_index = index - 1;
-				while (!note) {
-					if ( up_index >= 0 && up_index < FREQ_RES_PAYBACKS_NUM - 1 )
-						if( noteNameTable[up_index++] )
-							note = noteNameTable[up_index];
-					else if (down_index >= 0 && down_index < FREQ_RES_PAYBACKS_NUM - 1)
-						if (noteNameTable[down_index--])
-							note = noteNameTable[down_index];
-				}
-			}
+			if (!note)
+				note = get_nearest_note(index);
 			cout << note << endl;
 		}
-		this_thread::sleep_for(chrono::milliseconds(44));
+
+		//pause
+		this_thread::sleep_for(chrono::milliseconds( VISUALIZATION_PAUSE ));
 	}
 	return;
 }
 
 
-//gate to remove noise
-float trashold = 0.01;
+/*	Gate to remove noise
+*
+*	signal from 'gate_channel' would be processed
+*	gated signal is been put to 'buffer_for_fft'
+*/
 void filter_gate(int length) {
 	BASS_ChannelGetData(gate_channel, gate_buffer, length );
 	if (gate_buffer) {
 		for (int i = 0; i < length; i++) {
 			float val = gate_buffer[i];
-			if (abs(val) < trashold)
+			if (abs(val) < SIGNAL_GATE_TRASHOLD )
 				gate_buffer[i] = 0;
 		}
 	}
 	BASS_StreamPutData(buffer_for_fft, gate_buffer, length);
 }
 
+
 //CALLBACK
+
 DWORD CALLBACK inputProc(BOOL input, DWORD channel, void *buffer, DWORD length, void *user){
 	DWORD fftlen = min( length, 1024 * 2 * 2 ); // limit the data to the amount required by the FFT (1024 samples)
-	
-	if( ! c_freq_res )
-		c_freq_res = new float[FREQ_RES_PAYBACKS_NUM/2];
-	if (!gate_buffer)
-		gate_buffer = new float[FREQ_RES_PAYBACKS_NUM/2];
+	BASS_StreamPutData(gate_channel, buffer, fftlen);	//get signal
+	filter_gate(fftlen);								//Gate
+	BASS_ChannelGetData(buffer_for_fft, frequency_response_buffer, BASS_DATA_FFT8192);	//FFT
 
-	BASS_StreamPutData(gate_channel, buffer, fftlen);
-	filter_gate(fftlen);
-	BASS_ChannelGetData(buffer_for_fft, c_freq_res, BASS_DATA_FFT8192);
+	BASS_StreamPutData(left_channel_stream, buffer, fftlen );	//send signal to left headphone 
+	BASS_StreamPutData(right_channel_stream, buffer, fftlen );	//send signal to right headphone
 
-	BASS_StreamPutData(left_channel_stream, buffer, fftlen );
-	BASS_StreamPutData(right_channel_stream, buffer, fftlen );
-
-	c_buffer = buffer;
-	c_length = length;
-	samples_in_buffer++;
+	signal_graph_buffer = buffer;	//don't forget about buffer
 	return length;	
 }
 
 
 
 void main() {		
+
+	//Init
 	setlocale(LC_ALL, "Rus");	
 	if ( ! BASS_Init(-1, 44100, 0, 0, NULL)) {
 		cout << "BASS ERROR: " << BASS_ErrorGetCode() << endl;
@@ -198,12 +213,14 @@ void main() {
 
 	BASS_ASIO_GetInfo(&info);
 	GraphInit();	//init graph's settings	
+	set_notes();	//init arrays with notes
 
 	//set streams 
 	left_channel_stream = BASS_StreamCreate(BASS_ASIO_GetRate(), 1, BASS_STREAM_DECODE, STREAMPROC_PUSH, 0 );
 	right_channel_stream = BASS_StreamCreate(BASS_ASIO_GetRate(), 1, BASS_STREAM_DECODE, STREAMPROC_PUSH, 0 );
 	gate_channel = BASS_StreamCreate(BASS_ASIO_GetRate(), 1, BASS_STREAM_DECODE, STREAMPROC_PUSH, 0);
 	buffer_for_fft = BASS_StreamCreate(BASS_ASIO_GetRate(), 1, BASS_STREAM_DECODE, STREAMPROC_PUSH, 0);
+	
 	//outout channels
 	BASS_ASIO_ChannelEnableBASS(FALSE, 0, left_channel_stream, TRUE);
 	BASS_ASIO_ChannelEnableBASS(FALSE, 1, right_channel_stream, TRUE);
